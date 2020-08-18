29d886f0467ba30445c8c81b828178acb5ac7884
swissnez <neal@abovebeyondtech.se>
2020-08-18 20:34:23 +0200

    GetDate getFullYear()

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import ReactDOM from "react-dom";
 
 const myName = "Neal";
 
+let CurrentDate = new Date();
+
 ReactDOM.render(
   <div>
-    <h1>My practive attempt by {myName}</h1>
+    <h1 className="heading" contentEditable="true" spellCheck="flase">
+      My practive attempt by {myName}
+    </h1>
     <ul>
       <li>My lucky number {6 + 4}</li>
-      <li>React</li>
+      <li>React {}</li>
       <li>Attempt</li>
     </ul>
+    <h3>{CurrentDate.getFullYear()}</h3>
   </div>,
   document.getElementById("root")
 );
